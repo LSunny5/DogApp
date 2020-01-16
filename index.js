3b5755b61df3054883fed34df89c6a7aa0469f3a
Sunny Lee <leesunny507@gmail.com>
2020-01-16 03:01:51 -0500

    got first question working

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+//GET request for Dog Images
+function getDogImages(userNumber) {
+    let dogURL=`https://dog.ceo/api/breeds/image/random/${userNumber}`;
+    fetch(dogURL)
+        .then(response => response.json())
+        .then(responseJson => displayImages(responseJson))
+        .catch(error=>alert('Something is not working, please try again in a bit.'));
+ }
+ 
+ 
+
+
+
+
+
+
+//show the images
+ function displayImages(responseJson) {
+    
+    /************************************************** 
+     * Solution to Question 1 of Simple Get Requests
+     * Load images into console
+    ***************************************************/
+    console.log(responseJson);
+    /*******END SOLUTION for Question 1**********/
+
+    for (let one of responseJson.message) {
+        $('.imageSection').append(`<img src="${one}">`);
+    }
+
+   //$('.imageSection').html(results);
+
+
+
+   $('.results').removeClass('hidden');
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
+ //starting the app, after user inputs
+ function startApp() {
+   $('form').submit(event => {
+     event.preventDefault();
+     $('.results').addClass('hidden');
+     let nPics=$('#userNumber').val();
+     getDogImages(nPics);
+   });
+ }
+ 
+ //App ready and waiting for user to enter numbers
+ $(function() {
+    console.log("App is ready, waiting for user...");
+    startApp();
+ }); 
